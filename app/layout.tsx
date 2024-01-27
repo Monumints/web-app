23cc3e4e2b92adb6ca653ffda25cdfd4013cac1c
0xdorifto <gastaosousafaria@gmail.com>
2024-01-27 20:35:59 +0000

    Change font to montserrat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Monumints",
-  description: "Tokenize your trips!",
+  description: "Monumits: Tokenize your trips!",
 };
 
 export default function RootLayout({
@@ -15,8 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={montserrat.className}>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
+      <body>
+        <main className="flex justify-center items-center h-screen">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
